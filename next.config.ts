f7a1a72464047e45c94630628d8965f11068878e
Sakti Pardano <sakti.pardano29@gmail.com>
2026-07-03 10:57:33 +0700

    github page set up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Tells Next.js to generate static HTML/CSS/JS
+  basePath: isProd ? '/portfolio-spardano' : '', 
+  assetPrefix: isProd ? '/portfolio-spardano/' : '',
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js default image optimization
+  },
 };
 
 export default nextConfig;
