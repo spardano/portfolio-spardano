db6fb41d395fcb5c1604c8c07e15cd7ad1578be3
Sakti Pardano <sakti.pardano29@gmail.com>
2026-07-08 14:45:14 +0700

    update resume

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -33,7 +33,7 @@ export default function Hero() {
                             </svg>
                             {t("See My Projects", "Lihat Projek Pilihan")}
                         </a>
-                        <a href="/resume.pdf" download="Sakti_Pardano_Resume.pdf" className="flex items-center gap-2 border border-black text-black px-6 py-3 rounded-full font-medium hover:bg-gray-50 transition-all">
+                        <a href="/resume sakti pardano.pdf" download="Sakti_Pardano_Resume.pdf" className="flex items-center gap-2 border border-black text-black px-6 py-3 rounded-full font-medium hover:bg-gray-50 transition-all">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-4 h-4 text-gray-700">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
                             </svg>
